8691d4ae18d3602fcd12229fdad15240e54dc756
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2024-02-29 22:54:52 +0300

    feat(tools): добавил скрипты для Windows

--- tools/depends_windows.cmd
+++ tools/depends_windows.cmd
@@ -5,7 +5,18 @@ cls
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo Установка необходимых инструментов
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-choco install wget unzip -y
+choco install wget unzip pandoc python312 -y
+
+echo ~~~~~~~~~~~~~~~
+echo Установка Latex
+echo ~~~~~~~~~~~~~~~
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl.zip
+unzip install-tl.zip
+del install-tl.zip
+cd install-tl-*
+install-tl-windows.bat --scheme scheme-minimal -repository https://ctan.altspu.ru/systems/texlive/tlnet/
+tlmgr install xetex
+mkdir C:\texlive\2023\texmf-var\fonts\cache
 
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo Установка AnonymicePro Nerd Font
@@ -15,6 +26,15 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/AnonymousP
 unzip -o AnonymousPro.zip -d fonts
 cd fonts
 copy /Y *.ttf "C:\Windows\Fonts"
+cd ..
+
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Установка шрифтов для Latex
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+mkdir C:\texlive\2023\texmf-var\fonts\conf
+cd C:\texlive\2023\texmf-var\fonts\conf
+echo ^<dir^>C:/Windows/Fonts^</dir^> > fonts.conf
+fc-cache -vf
 
 echo ~~~~~~~~~~~~~~~~~~
 echo Установка языка R
@@ -22,12 +42,17 @@ echo ~~~~~~~~~~~~~~~~~~
 wget https://cloud.r-project.org/bin/windows/base/R-4.3.2-win.exe
 call R-4.3.2-win.exe
 
-echo ~~~~~~~~~~~~~~~~~~~
-echo Установка RMarkdown
-echo ~~~~~~~~~~~~~~~~~~~
-cd "C:\Program Files\R\R-4.3.2\bin"
-Rscript.exe -e "install.packages('fastmap', repos='https://cloud.r-project.org')"
-Rscript.exe -e "install.packages('rmarkdown', repos='https://cloud.r-project.org')"
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Установка переменных окружения для R
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+set PATH=%PATH%;"C:\Program Files\R\R-4.3.2\bin"
+
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Установка RMarkdown и необходимых компонентов для его работы
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Rscript -e "install.packages('fastmap', repos='https://cloud.r-project.org')"
+Rscript -e "install.packages('rmarkdown', repos='https://cloud.r-project.org')"
+Rscript -e "install.packages('reticulate', repos='https://cloud.r-project.org')"
 
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo Удаление остаточных файлов
---
+++ tools/render_pdf_windows.cmd
@@ -0,0 +1,9 @@
+@echo off
+PATH=%PATH%;"C:\Program Files\R\R-4.3.2\bin\x64"
+mkdir pdfs
+for /F %%G in ('dir *.rmd /s /b') do (
+set "F=%%G"
+setlocal EnableDelayedExpansion
+Rscript --encoding=UTF-8 -e "rmarkdown::render('!F:\=/!', output_dir='pdfs');"
+)
+@echo on
