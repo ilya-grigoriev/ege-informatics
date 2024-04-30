e49a295988d2e58a1825f3a0986b6ae0d3c82487
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-04-30 21:30:59 +0300

    feat(depends_linux): приукрасил скрипт для Linux

--- tools/depends_linux.sh
+++ tools/depends_linux.sh
@@ -16,18 +16,20 @@ echo ""
 echo "~~~~~~~~~~~~~~~~~~~"
 echo "Установка RMarkdown"
 echo "~~~~~~~~~~~~~~~~~~~"
-if ! [ $(Rscript -e "system.file(package='rmarkdown')") ]; then
+if ! [[ $(Rscript -e "system.file(package='rmarkdown')") ]]; then
     Rscript -e "install.packages('rmarkdown', repos='http://cran.us.r-project.org')"
     Rscript -e "install.packages('reticulate', repos='http://cran.us.r-project.org')"
     Rscript -e "reticulate::virtualenv_remove()"
     Rscript -e "reticulate::virtualenv_create()"
+else
+    echo "RMarkdown уже установлен"
 fi
 
 echo ""
 echo "~~~~~~~~~~~~~~~"
 echo "Установка Latex"
 echo "~~~~~~~~~~~~~~~"
-if ! [ "/usr/bin/latex" ]; then
+if ! [ -d "/usr/local/texlive" ]; then
     wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
     zcat < install-tl-unx.tar.gz | tar xf -
     rm -rf install-tl-unx.tar.gz*
@@ -35,6 +37,8 @@ if ! [ "/usr/bin/latex" ]; then
     sudo perl ./install-tl --no-interaction --scheme scheme-medium
     sudo tlmgr install tcolorbox hyperref amsmath setspace indentfirst array
     rm -rf install-tl-*
+else
+    echo "Latex уже установлен"
 fi
 
 
@@ -42,12 +46,17 @@ echo ""
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Установка AnonymicePro Nerd Font"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-if ! [ $(fc-list | grep "Anonymice") ]; then
+if ! [[ $(fc-list | grep "Anonymice") ]]; then
+    rm -rf /tmp/fonts
+    rm -rf AnonymousPro.zip*
+
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/AnonymousPro.zip
     unzip AnonymousPro.zip -d fonts
-    rm -rf AnonymousPro.zip
+    rm -rf AnonymousPro.zip*
     cd fonts
     sudo cp *.ttf /usr/share/fonts
+else
+    echo "Шрифт AnonymicePro Nerd Font уже есть"
 fi
 
 echo ""
